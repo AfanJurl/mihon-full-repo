594c51dd04481c22daac2e2d5e1bd40c8982111a
AfanJurl <81816023+AfanJurl@users.noreply.github.com>
2026-03-15 13:43:51 +0300

    Add GManga extension for Tachiyomi
    Implement GManga extension for Tachiyomi.

---
+++ src/arabic/gmanga/src/arabic/gmanga/src/arabic/gmanga/res/values/src/arabic/gmanga/src/eu/kanade/tachiyomi/extension/ar/gmanga/GManga.kt
@@ -0,0 +1,12 @@
+package eu.kanade.tachiyomi.extension.ar.gmanga
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class GManga : Madara(
+    "GManga",
+    "https://gmanga.me",
+    "ar",
+    SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()),
+)
