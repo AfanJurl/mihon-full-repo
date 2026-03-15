4e12bb0051cd248776291f83c215bd23228b8e0a
AfanJurl <81816023+AfanJurl@users.noreply.github.com>
2026-03-15 13:51:42 +0300

    Delete src/english/SOURCEID directory

--- src/english/SOURCEID/src/eu/kanade/tachiyomi/extension/en/SOURCEID/CLASSNAME.kt
+++
@@ -1,6 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.SOURCEID
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-class CLASSNAME : Madara("SOURCENAME", "BASEURL", "en",
-    SimpleDateFormat("MMMM dd, yyyy", Locale.US))
